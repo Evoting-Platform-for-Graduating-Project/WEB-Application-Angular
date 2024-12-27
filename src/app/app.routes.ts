9590c0aaaf6b265a268b954cda9990355c84b50d
Dawid Samsonik <samsonikdawid@gmail.com>
2024-12-27 15:37:09 +0100

    navbar i routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '';
+    pathMatch: 'full',
+    loadComponent() {
+        return import('./')
+    },
+}];
