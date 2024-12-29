65e9f5edf316ff79571dbaebd1f4ea6c63ebfbf4
Dawid Samsonik <samsonikdawid@gmail.com>
2024-12-29 23:42:56 +0100

    fix

---
+++ src/app/components/timer/timer.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit} from '@angular/core';
+import { CountdownConfig, CountdownModule} from 'ngx-countdown';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  imports: [CountdownModule],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.css'
+})
+export class TimerComponent implements OnInit {
+  @Input() deadline!: Date;
+  countdownConfig: CountdownConfig = {
+    leftTime: 0,
+    format: 'HH:mm:ss',
+  };
+
+  ngOnInit() {
+    this.updateCountdown();
+  }
+
+  private updateCountdown() {
+    if (this.deadline) {
+      const now = new Date().getTime();
+      const deadlineTime = new Date(this.deadline).getTime();
+      const diffInSeconds = Math.max(Math.floor((deadlineTime - now) / 1000), 0);
+
+      this.countdownConfig = {
+        ...this.countdownConfig,
+        leftTime: diffInSeconds
+      };
+    }
+  }
+}
