95df255784650c035f23606a6992e0ef6feb34cc
Dawid Samsonik <samsonikdawid@gmail.com>
2024-12-30 15:43:10 +0100

    added upcoming votes component adjusted app component

---
+++ src/app/components/upcomingvote/upcomingvote.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upcomingvote',
+  imports: [],
+  templateUrl: './upcomingvote.component.html',
+  styleUrl: './upcomingvote.component.css'
+})
+export class UpcomingvoteComponent {
+
+}
