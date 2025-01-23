5b5bdeff96bd9fd2d0ff10a21b1c1f6b44352402
Dawid Samsonik <samsonikdawid@gmail.com>
2025-01-23 20:21:22 +0100

    added voting component adjusted active vote component to add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { LeftNavComponent } from './components/left-nav/left-nav.component';
 import { ActivevoteComponent } from './components/activevote/activevote.component';
 import {UpcomingvoteComponent} from './components/upcomingvote/upcomingvote.component';
-import { RouterOutlet} from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LeftNavComponent, ActivevoteComponent, UpcomingvoteComponent, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [HeaderComponent, LeftNavComponent, ActivevoteComponent, UpcomingvoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
