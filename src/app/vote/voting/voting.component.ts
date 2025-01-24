094b3f2ff33fd198e0060a173885244e1a79ad55
Dawid Samsonik <samsonikdawid@gmail.com>
2025-01-24 02:56:54 +0100

    added confirmation popup

--- src/app/vote/voting/voting.component.ts
+++ src/app/vote/voting/voting.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { NgFor } from '@angular/common';
+import { NgIf} from '@angular/common';
 import { NgClass} from '@angular/common';
 
 
 @Component({
   selector: 'app-voting',
-  imports: [FormsModule, NgFor, NgClass],
+  imports: [FormsModule, NgFor, NgClass, NgIf],
   templateUrl: './voting.component.html',
   styleUrl: './voting.component.css'
 })
@@ -18,10 +19,20 @@ export class VotingComponent {
     {id: 4, name: 'KONONOWICZ KRZYSZTOF', party: 'Nazwa partii'},
   ];
   selectedCandidate: number  | null = null;
+  showPopUp: boolean = false;
+
+  openConfirmation () {
+    this.showPopUp = true;
+  }
+
+  closeConfirmation () {
+    this.showPopUp = false;
+  }
 
   submitVote() {
     if (this.selectedCandidate) {
       alert(`Oddano głos na: ${this.selectedCandidate}`);
+      this.closeConfirmation()
     }
   }
 
