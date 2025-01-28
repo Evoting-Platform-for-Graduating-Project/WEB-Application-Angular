e4658025934159fc012df5228b0498f37483fdd4
Dawid Samsonik <samsonikdawid@gmail.com>
2025-01-29 00:51:12 +0100

    angular material rewrite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { LeftNavComponent } from './components/left-nav/left-nav.component';
 import { RouterOutlet } from '@angular/router';
-
+import { NavigationComponent} from './components/navigation/navigation.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LeftNavComponent, RouterOutlet],
+  imports: [HeaderComponent, LeftNavComponent, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/activevote/activevote.component.ts
+++ src/app/components/activevote/activevote.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TimerComponent } from '../timer/timer.component';
 import {RouterLink} from '@angular/router';
 import {RouterOutlet} from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-activevote',
   standalone: true,
-  imports: [TimerComponent, RouterLink, RouterOutlet],
+  imports: [TimerComponent, RouterLink, RouterOutlet, MatCardModule],
   templateUrl: './activevote.component.html',
   styleUrl: './activevote.component.css'
 })
